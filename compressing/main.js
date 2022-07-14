dec1a4dd58e8e7e119a1da2b9991240a7d682eaf
admin <1596782257@qq.com>
2022-07-14 22:04:42 +0800

    submit

--- compressing/main.js
+++ compressing/main.js
@@ -1,164 +1,151 @@
 
-// 解压文件，压缩文件
-const fs = require("fs")
-const path = require('path');
-let pathName = "D:/workSpace/renameFile/data"
-const cp = require("child_process");
-
-function unZIP(winRarPath, zipFilePath, unZipFolder) {
-    console.log(winRarPath+'\n'+ zipFilePath+'\n'+ unZipFolder)
-    return new Promise(async (resolve, reject) => {
-        cp.execFile(winRarPath, ["x", "-inul", zipFilePath, unZipFolder], function (err, stdout, stderr) {
-            if (err) {
-                reject(err)
-            }
-            
-                resolve(stdout)
-            
-        })
-    })
-}
-
-function ZIP(winRarPath, zipFilePath, unZipFolder) {
-    //zipFilePath 压缩path   unZipFolder 压缩文件
-    return new Promise(async (resolve, reject) => {
-        cp.execFile(winRarPath, ["a", "-inul", zipFilePath, unZipFolder], function (err, stdout, stderr) {
-            if (err) {
-                reject(err)
-            }
-            resolve(stdout)
-        })
-    })
-}
-
-
-async function unZipToFolderList(){
-  return await new Promise(async (resolve , reject) => {
-    let paths = []
-    let pathscallback = []
-    let files = fs.readdirSync(pathName)
-    for (let i = 0; i < files.length; i++) {
-      const fileArr = files[i].split('.');
-      if (fileArr[1] == 'rar' || fileArr[1] == 'zip') {
-        let path = pathName + '/' + files[i]
-        paths.push(path)
-        //console.log(path);
+/**
+ * 解压文件，压缩文件
+ */
+ const fs = require("fs")
+ const path = require('path');
+ const cp = require("child_process");
+ const { inherits } = require("util");
+ 
+ let WinRARPath = "D:/Program Files/WinRAR/WinRAR.exe"
+ let pathName = "D:/workSpace/renameFile/data" //压缩文件路径
+ let pathName_backup = "D:/workSpace/renameFile/data/Backup"  //解压过的文件存放路径
+ let pathName_Pack = "D:/workSpace/renameFile/data/Pack"  //解压完成 修改文件名  后的文件
+ let pathName_ZIP = "D:/workSpace/renameFile/data/Pack/PackZIP"  //最后压缩 所有文件的路径
+ 
+ 
+ function unZIP(winRarPath, zipFilePath, unZipFolder) {
+     return new Promise(async (resolve, reject) => {
+         cp.execFile(winRarPath, ["x", "-inul", zipFilePath, unZipFolder], function (err, stdout, stderr) {
+             if (err) {
+                 reject(err)
+             }
+             resolve(stdout)
+         })
+     })
+ }
+ 
+ function ZIP(winRarPath, zipFilePath, unZipFolder) {
+     return new Promise(async (resolve, reject) => {
+         cp.execFile(winRarPath, ["a", "-inul", zipFilePath, unZipFolder], function (err, stdout, stderr) {
+             if (err) {
+                 reject(err)
+             }
+             resolve(stdout)
+         })
+     })
+ }
+ 
+ async function unZipToFolderList(){
+   return await new Promise(async (resolve , reject) => {
+     try {
+       let zipPathList = []   //压缩文件集合
+       let unzipPathBackcall = []   // 解压完后的文件集合
+       let files = fs.readdirSync(pathName)
+       for (let i = 0; i < files.length; i++) {
+         const fileSuffix=path.extname(files[i])  //Suffix
+         if (fileSuffix === '.rar' || fileSuffix === '.zip') {
+           let path = pathName + '/' + files[i]
+           zipPathList.push(path)
+         }
+       }
+       
+       for (let index = 0; index < zipPathList.length; index++) {
+           let basename = (path.basename(zipPathList[index])).substring(0,(path.basename(zipPathList[index])).lastIndexOf(".")) //basename 文件全名>>> 截取最后一个点去掉后缀名
+           let basenameList = basename.split("_")  //下划线 分割成 List
+           let unzipPath = pathName_Pack+"/"+ basenameList[5]+ "_"+basenameList[4]; //
+           if (fs.existsSync(zipPathList[index])) {
+             if (!fs.existsSync(unzipPath)) {
+               fs.mkdirSync(unzipPath);
+             }
+             await unZIP(WinRARPath,zipPathList[index], unzipPath);
+             
+             var data = fs.readFileSync(zipPathList[index]);// 读取
+             fs.writeFileSync(pathName_backup +"/"+ path.basename(zipPathList[index]), data); //写入
+             fs.unlinkSync(zipPathList[index]);// 并且删除原文件
+ 
+             unzipPathBackcall.push(unzipPath)
+           }
+       }
+       resolve(unzipPathBackcall)  //返回解压的路径集合
+     } catch (error) {
+       reject(new Error(error))
+     }
+   })
+ }
+ async function modifyFile(filename) {
+   for (let index = 0; index < filename.length; index++) {
+     readDirAllFile(filename[index])
+   }
+ 
+   for (let index = 0; index < File.Item.length; index++) {
+     const element = File.Item[index];
+     const compress = element.name.substring(0, element.name.lastIndexOf("."))
+     const compressPath1 = element.path.substring(0, element.path.lastIndexOf("\\"))
+     const compressPath2 = compressPath1.substring(0, compressPath1.lastIndexOf("\\"))
+ 
+     if (fs.existsSync(pathName_ZIP)) {
+      await ZIP(WinRARPath, pathName_ZIP + "\\" + compress,compressPath2);
+     }
+ 
+   }
+ }
+ 
+ const File = { Item: [] }
+ function readDirAllFile(filePath) {
+     // 待优化 >>> 多个文件已最大那个命名
+     let files = fs.readdirSync(filePath);
+     for (const file of files) {
+       let fileTruePath = path.join(filePath, file);
+       if (fs.existsSync(fileTruePath)) {
+         let stats = fs.statSync(fileTruePath)
+         if (stats.isDirectory()) {
+            readDirAllFile(fileTruePath); //递归
+         } else if(stats.isFile()) {
+           let reg = file.substring(file.length, file.length - 8)
+           if (reg === 'dlog.txt') {
+               const fixPathName = file.substring(0, file.lastIndexOf("."))
+               const fixPath = fileTruePath.substring(0, fileTruePath.lastIndexOf('\\'))
+               let fixPathNew = fixPath.substring(0, fixPath.lastIndexOf('\\')) + "\\" + fixPathName
+               const _File = { path: '', name: '' }
+               _File.name = file
+               _File.path = fixPathNew
+               File.Item.push(_File)
+               fs.renameSync(fixPath,fixPathNew)
+             
+           }
+         }
+       }
+     }
+ }
+ 
+ function init(){
+   try {
+     console.log(`************************************************** \n  WinRAR path: ${WinRARPath} \n  Path: ${pathName} \n  备份路径: ${pathName_backup} \n  打包路径：${pathName_Pack} \n************************************************** `)
+     if(!fs.existsSync(pathName_backup)){
+       fs.mkdirSync(pathName_backup);
+     }
+     if(!fs.existsSync(pathName_Pack)){
+       fs.mkdirSync(pathName_Pack);
+     }
+     if(!fs.existsSync(pathName_ZIP))
+      {
+        fs.mkdirSync(pathName_ZIP);
       }
-      else {
-        //console.log(pathName +files[i] +"------------"+ pathName +"/"+ "backup");
-        //fs.copyFile(pathName +"/"+files[i], pathName +"/"+ "backup")
-        var deletFile = pathName + '/' + files[i]
-        //fs.unlinkSync(deletFile)
-        console.log(`${files[i]}文件不符合压缩要求，已经被成功删除`);
-      }
-    }
-    
-    for (let index = 0; index < paths.length; index++) {
-        let pathundecompress
-        let pathSummary = []
-        let configStr = paths[index].split("\n")
-        //console.log(configStr);
-        for (let index = 0; index < configStr.length; index++) {
-          let str1 = configStr[index].substring(0, configStr[index].lastIndexOf("_"))
-          let str2 = str1.substring(str1.length, str1.lastIndexOf("_") + 1)
-          let str3 = configStr[index].substring(configStr[index].lastIndexOf("."), configStr[index].lastIndexOf("_"))
-          pathundecompress =paths[index].substring(0,paths[index].lastIndexOf("/")) + "/" +str2 + str3
-        
-          if (fs.existsSync(paths[index])) {
-            if (!fs.existsSync(pathundecompress)) {
-              fs.mkdirSync(pathundecompress);
-              pathSummary.push(pathundecompress)
-            }
-            await unZIP("D:/rar软件/WinRAR.exe",configStr[index], pathundecompress);
-            pathscallback.push(pathundecompress)
-          }
-        }
-    }
-    resolve(pathscallback)
-  })
-}
-
-
-function modifyFile(filename){
-    for (let index = 0; index < filename.length; index++) {
-        readDirAllFile(filename[index])
-      }
-      console.log(File.Item)
-    
-      for (let index = 0; index < File.Item.length; index++) {
-        const element = File.Item[index];
-        const compress =  element.name.substring(0,element.name.lastIndexOf("."))
-        const compressPath1 = element.
-        path.substring(0,element.path.lastIndexOf("\\"))
-        const compressPath2 = compressPath1.substring(0,compressPath1.lastIndexOf("\\"))
-        const compressPath3 = compressPath2.substring(0,compressPath2.lastIndexOf("\\"))
-        const compressPath4 = compressPath3.substring(0,compressPath3.lastIndexOf("\\"))
-    
-        //console.log(compressPath2 + compress); //压缩路径                 compressPath3 压缩文件路径
-        console.log(compressPath2 +"------" + compressPath4 + "\\" +"backup"+ "\\"+compress+'.zip');
-    
-        if (!fs.existsSync(compressPath4 + "\\backup")) {
-          fs.mkdirSync(compressPath4 + "\\backup");
-        }
-        if(fs.existsSync(compressPath4 + "\\backup")) {
-          ZIP("D:/rar软件/WinRAR.exe",
-          compressPath4 +  "\\" +"backup"+ "\\" + compress,
-          compressPath2);
-          }
-      } 
-}
-
-
-const File = {
-    Item: []
-  }
-
-function readDirAllFile(filePath) {
-
-    let allFile = {};
-    let files = fs.readdirSync(filePath);
-  
-    for (const file of files) {
-      let fileTruePath = path.join(filePath, file);
-      if (fs.existsSync(fileTruePath)) {
-        let stats = fs.statSync(fileTruePath)
-  
-        if (stats.isDirectory()) {
-          allFile[file] = readDirAllFile(fileTruePath);
-        } else if(stats.isFile()) {
-          let reg = file.substring(file.length, file.length - 8)
-          if (reg === 'dlog.txt') {
-            const fixPathName = file.substring(0, file.lastIndexOf("."))
-            const fixPath = fileTruePath.substring(0, fileTruePath.lastIndexOf('\\'))
-            //解压的路径应存放在数组中,循环改文件名字以及压缩文件路径
-            let fixPathNew = fixPath.substring(0, fixPath.lastIndexOf('\\')) + "\\" + fixPathName
-            const _File = {
-              path: '',
-              name: ''
-            }
-  
-            _File.name = file
-            _File.path = fixPathNew
-            File.Item.push(_File)
-            
-            
-            fs.renameSync(
-              fixPath,
-              fixPathNew
-            )
-          }
-        }
-  
-      }
-    }
-  }
-
-
-async function test(){
-
-    const re = await unZipToFolderList()
-    console.log(re)
-    modifyFile(re)
-}
-test() 
-
+   } catch (error) {
+     console.log('init Folder error')
+   }
+ }
+ 
+ async function main(){
+     init()
+     console.log("解压中>>>")
+     const rsp = await unZipToFolderList()
+     console.log("解压完成!")
+     console.log("打包文件中>>>")
+     await modifyFile(rsp)
+     console.log("打包压缩成功!")
+ }
+ main() 
+ 
+ 
