fee7092032b8644f52c78ae0216b7815ace3e06a
admin <1596782257@qq.com>
2022-10-31 21:12:55 +0800

    modified:   compressing/new.js

--- compressing/new.js
+++ compressing/new.js
@@ -2,90 +2,93 @@
 /**
  * 解压文件，压缩文件
  */
- const fs = require("fs")
- const path = require('path');
- const compressing = require('compressing');
+const fs = require("fs")
+const path = require('path');
+const compressing = require('compressing');
 
- let pathName = "D:/workSpace/renameFile/data" //原数据储存路径
- let pathName_ZIP = "D:/workSpace/renameFile/Packunzip"  //最后压缩 所有文件的路径
- let pathName_backup = "D:/workSpace/renameFile/backup"
- let pathName_Exception = "D:/workSpace/renameFile/Exception"
+let pathName = "D:/workSpace/renameFile/data" //原数据储存路径
+let pathName_ZIP = "D:/workSpace/renameFile/Packunzip"  //最后压缩 所有文件的路径
+let pathName_backup = "D:/workSpace/renameFile/backup"
+//  let pathName_Exception = "D:/workSpace/renameFile/Exception"
 
 
- function compress(compression){
+async function compress(compression) {
   return new Promise(function (resolve, reject) {
-      const tarStream = new compressing.zip.Stream();
-//      console.log(path.join(pathName,compression));
-      console.log('start--' + new Date());
-      var filelist = fs.readdirSync(path.join(pathName,compression))
-      let fileName = ''
-      for (let i = 0; i < filelist.length; i++) {
-        let reg = filelist[i].substring(filelist[i].length, filelist[i].length - 8)
-        if (reg === 'dlog.txt') { // size issue
-          fileName = filelist[i]
-        }
-          tarStream.addEntry(path.join(pathName,compression,filelist[i]));
+    const tarStream = new compressing.zip.Stream();
+    //      console.log(path.join(pathName,compression));
+    
+    var filelist = fs.readdirSync(path.join(pathName, compression))
+    if (filelist.length <= 0) {
+      //console.log(path.join(pathName, compression))
+      fs.rmdirSync(path.join(pathName, compression))
+      resolve('nullFolder')
+      return
+    }
+    let fileName = ''
+    for (let i = 0; i < filelist.length; i++) {
+      //let reg = filelist[i].substring(filelist[i].length, filelist[i].length - 8)
+      if (filelist[i].endsWith('dlog.txt')) { // size issue
+        fileName = filelist[i]
       }
-      let compressionFile = ''
-      if (fileName === '') {
-         compressionFile = path.join(pathName_Exception,compression) + '.zip'  
-      } else {
-         compressionFile = path.join(pathName_ZIP,fileName) + '.zip'  
-      }
-      
-      const destStream = fs.createWriteStream(compressionFile);
-      let handleError = function (err) {
-          reject(err);
-      }
-      let finished = function(){
-        filesOperation(compression).then(() => {
-          resolve(compressionFile);
-        })
-      }
-      tarStream.on('error', handleError).on('end',finished).pipe(destStream);
+      tarStream.addEntry(path.join(pathName, compression, filelist[i]));
+    }
+    let compressionFile = ''
+    if (fileName === '') {
+      compressionFile = path.join(pathName_ZIP, compression) + '.zip'
+    } else {
+      compressionFile = path.join(pathName_ZIP, fileName) + '.zip'
+    }
+
+    const destStream = fs.createWriteStream(compressionFile);
+    let handleError = function (err) {
+      reject(err);
+    }
+    let finished = function () {
+      resolve(compressionFile);
+
+    }
+    tarStream.on('error', handleError).on('end', finished).pipe(destStream);
 
   });
 }
 
 
 async function filesOperation(compression) {
-  return await new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
-                // -------搬
+      // -------搬
 
-                let files = fs.readdirSync(path.join(pathName,compression));
-                if(!fs.existsSync(path.join(pathName_backup,compression)))
-                   {
-                     fs.mkdirSync(path.join(pathName_backup,compression));
-                   }
-                
-                for (let index = 0; index < files.length; index++) {
-                  let fileTruePath = path.join(pathName, compression,files[index]);
-                  if (fs.existsSync(fileTruePath)) {
-                    let stats = fs.statSync(fileTruePath)//异步返回文件信息
-                    if (stats.isDirectory()) {
-                       //readDirAllFile(fileTruePath); //递归
-                    } else if(stats.isFile()) {
-                        fs.rename((path.join(path.join(pathName,compression),files[index])),(path.join(pathName_backup,compression,files[index])) ,(err)=>{
-                          if(err){
-                            console.log(err)
-                          }else{
-                            if(index===files.length-1){
-                              fs.rmdirSync(path.join(pathName,compression));
-                              console.log('end--' + new Date());
-                            }
-                            //console.log('剪切到js文件夹内了')
-                            
-                          }
-                        })
-              
-                    }
-                  }
+      let files = fs.readdirSync(path.join(pathName, compression));
+      if (!fs.existsSync(path.join(pathName_backup, compression))) {
+        fs.mkdirSync(path.join(pathName_backup, compression));
+      }
+
+      for (let index = 0; index < files.length; index++) {
+        let fileTruePath = path.join(pathName, compression, files[index]);
+        if (fs.existsSync(fileTruePath)) {
+          let stats = fs.statSync(fileTruePath)//异步返回文件信息
+          if (stats.isDirectory()) {
+            //readDirAllFile(fileTruePath); //递归
+          } else if (stats.isFile()) {
+            fs.rename((path.join(path.join(pathName, compression), files[index])), (path.join(pathName_backup, compression, files[index])), (err) => {
+              if (err) {
+                console.log(err)
+              } else {
+                if (index === files.length - 1) {
+                  fs.rmdirSync(path.join(pathName, compression));
                 }
-      
-                //----------
+                 //console.log('剪切到js文件夹内了')
 
-                resolve('Success')
+              }
+            })
+
+          }
+        }
+      }
+
+      //----------
+
+      resolve('Success')
     } catch (error) {
       reject(error)
     }
@@ -94,20 +97,18 @@ async function filesOperation(compression) {
 }
 
 
-function init(){
+function init() {
   try {
-    if(!fs.existsSync(pathName_ZIP))
-     {
-       fs.mkdirSync(pathName_ZIP);
-     }
-     if(!fs.existsSync(pathName_backup))
-     {
-       fs.mkdirSync(pathName_backup);
-     }
-     if(!fs.existsSync(pathName_Exception))
-     {
-       fs.mkdirSync(pathName_Exception);
-     }
+    if (!fs.existsSync(pathName_ZIP)) {
+      fs.mkdirSync(pathName_ZIP);
+    }
+    if (!fs.existsSync(pathName_backup)) {
+      fs.mkdirSync(pathName_backup);
+    }
+    //  if(!fs.existsSync(pathName_Exception))
+    //  {
+    //    fs.mkdirSync(pathName_Exception);
+    //  }
   } catch (error) {
     console.log('init Folder error')
   }
@@ -119,17 +120,22 @@ let Task = async () => {
   打包路径：${pathName_ZIP} \n
   ************************************************** `)
   init()
-  let  Files  =fs.readdirSync(pathName)  
-for (let index = 0; index < Files.length; index++) { 
+  let Files = fs.readdirSync(pathName)
+  console.time('start');
+  for (let index = 0; index < Files.length; index++) {
+    let re = await compress(Files[index])
+    if (!(re === 'nullFolder')) {
+      await filesOperation(Files[index])
+    }
 
-  compress(Files[index])
-}
-if (Files.length == 0) {
-  console.log("文件夹为空")
-} else {
-  console.log("正在压缩")
-}
+
+  }
+  console.timeEnd('start');
+  if (Files.length == 0) {
+    console.log("文件夹为空")
+  } else {
+    console.log("complete !")
+  }
 }
 
 Task()
- 
